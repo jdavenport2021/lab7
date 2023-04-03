668e57d22d686f7b0da49a1f554af49290d28297
Joshua Davenport <87725934+jdavenport2021@users.noreply.github.com>
2023-04-03 18:21:49 -0500

    Changed script.js to meet assignment requirements
    Attempt 1

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 function init(){
-//add your javascrip between these two lines of code
- 
+//add your javascript between these two lines of code
+  var button = document.getElementById("entrybutton");
+  var textfield = document.getElementById("entryinput");
+  var output = document.getElementById("textoutput");
 
+  button.addEventListener('click', pressed)
 
-
-
-
-
-
+  function pressed() {
+  	alert("Joshua Davenport: "+textfield.value);
+  	output.innerHTML = textfield.value;
+  };
+}
 window.addEventListener('load', init);
